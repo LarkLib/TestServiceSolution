9bfa7a48a0073744f8cce9f02fcf74269d73860d
Elite <Elite@test.com>
2020-04-10 12:15:07 +0800

    remove alihealth test code

--- TestAliHealthConsoleApp/Program.cs
+++ TestAliHealthConsoleApp/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Top.Api;
+using Top.Api.Request;
+using Top.Api.Response;
+
+namespace TestAliHealthConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
